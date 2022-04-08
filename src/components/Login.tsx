acdd114cdf87605c2bc66c306ad707760f24b348
EnriqueCazares <cazaresenrique97@gmail.com>
2022-04-07 23:04:03 -0500

    Section 2 Finished

---
+++ src/components/Login.tsx
@@ -0,0 +1,79 @@
+import React, { SyntheticEvent } from "react";
+import { AuthService } from "../services/AuthService";
+
+interface LoginProps {
+  authService: AuthService;
+}
+
+interface LoginState {
+  userName: string;
+  password: string;
+  loginAttempted: boolean;
+  loginSuccess: boolean;
+}
+
+interface CustomEvent {
+  target: HTMLInputElement;
+}
+
+export class Login extends React.Component<LoginProps, LoginState> {
+  state: LoginState = {
+    userName: "",
+    password: "",
+    loginAttempted: false,
+    loginSuccess: false,
+  };
+
+  private setUserName(event: CustomEvent) {
+    this.setState({ userName: event.target.value });
+  }
+
+  private setPassword(event: CustomEvent) {
+    this.setState({ password: event.target.value });
+  }
+
+  private async handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    this.setState({ loginAttempted: true });
+    const result = await this.props.authService.login(
+      this.state.userName,
+      this.state.password
+    );
+    if (result) {
+      this.setState({ loginSuccess: true });
+    } else {
+      this.setState({ loginSuccess: false });
+    }
+  }
+
+  render() {
+    let loginMessage: any;
+    if (this.state.loginAttempted && this.state.loginSuccess) {
+      loginMessage = <label>Login Succesful</label>;
+    } else {
+      loginMessage = <label>Login Failed</label>;
+    }
+
+    return (
+      <div>
+        <h2>Por favor, inicia sesión</h2>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            type="text"
+            value={this.state.userName}
+            onChange={(e) => this.setUserName(e)}
+          />
+          <br />
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={(e) => this.setPassword(e)}
+          />
+          <br />
+          <input type="submit" value="Iniciar Sesión" />
+        </form>
+        {loginMessage}
+      </div>
+    );
+  }
+}
